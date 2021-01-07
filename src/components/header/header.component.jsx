2e2693a6a3a41ec79a5f97e24e1833e4b4227372
Queenie <qcmaganse@gmail.com>
2021-01-07 12:52:35 +0800

    modified header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {ReactComponent as Logo} from '../../assets/mug.svg'
 
 import './header.styles.scss'
 
 const Header = () => (
     <div className="header">
-        <Link className="logo-name" to="/">
-            <h2>MUG & SEE</h2>
+        <Link className="logo-container" to="/">
+            <Logo className="logo" />
         </Link>
+        <h1 className="logo-title">Mug and See</h1>
         <div className="options">
             <Link className="option" to="/shop">SHOP</Link>
             <Link className="option" to="/contact">CONTACT</Link>
