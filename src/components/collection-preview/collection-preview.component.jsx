e6d6e798f8ab8caeb01ed4d7aee048e065e1eb60
Queenie <qcmaganse@gmail.com>
2021-01-28 11:24:50 +0800

    updated cart utils to group items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({title, items}) => (
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.map(({ id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                items.map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
